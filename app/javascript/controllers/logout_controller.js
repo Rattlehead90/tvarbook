1c386d0a0ad76dd3e7913d93e8ae3d73ccf2e184
Rattlehead90 <strogonov@seznam.cz>
2022-12-22 18:03:38 +0100

    Add edit user to pop window

--- app/javascript/controllers/logout_controller.js
+++ app/javascript/controllers/logout_controller.js
@@ -9,17 +9,27 @@ export default class extends Controller {
 
     this.stateValue++;
     if (this.stateValue % 2 != 0) {
+      const pop = document.createElement('div')
       const logout_pop = document.createElement('a');
-      logout_pop.id = 'pop';
-      logout_pop.style.position = 'absolute';
+      const edit_pop = document.createElement('a');
+      pop.id = 'pop';
+      pop.style.position = 'absolute';
       logout_pop.href = '/users/sign_out';
+      edit_pop.href = '/users/edit';
       logout_pop.textContent = 'Log out';
-      logout_pop.classList.add("shadow-xl", "border-2", "border-gray-50", "bg-white", 
-                               "p-2", "w-48", "right-0", "top-12", "rounded-xl", "hover:bg-gray-300")
-      avatar.appendChild(logout_pop);
+      edit_pop.textContent = 'Edit profile';
+      pop.classList.add("shadow-xl", "border-2", "border-gray-50", "bg-white", 
+                               "p-2", "w-48", "right-0", "top-12", "rounded-xl",
+                               "flex", "flex-col");
+      logout_pop.classList.add("hover:bg-gray-300", "rounded-xl", "p-2");
+      edit_pop.classList.add("hover:bg-gray-300", "rounded-xl", "p-2");
+
+      pop.appendChild(edit_pop);
+      pop.appendChild(logout_pop);
+      avatar.appendChild(pop);
     } else {
-      const logout_pop = document.getElementById('pop');
-      avatar.removeChild(logout_pop);
+      const pop = document.getElementById('pop');
+      avatar.removeChild(pop);
     }
   }
 }
