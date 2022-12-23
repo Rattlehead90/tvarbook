14b454cd495c3ea0dbe1bf1ddfc526a448545610
Rattlehead90 <strogonov@seznam.cz>
2022-12-23 23:03:50 +0100

    Add Friendship mechanics

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :posts
+
+  # Friendship mechanics
+  has_many :invitations
+  has_many :pending_invitations, -> { where confirmed: false }, class_name: 'Invitation',
+                                                                foreign_key: 'friend_id'
+
+  def friends
+    invited_by_user = Invitation.where(user_id: id, confirmed: true).pluck(:friend_id)
+    invite_user = Invitation.where(friend_id: id, confirmed: true).pluck(:user_id)
+    ids = invited_by_user + invite_user
+    User.where(id: ids)
+  end
+
+  def friends_with?(user)
+    Invitation.confirmed_record?(id, user.id)
+  end
+
+  def send_invitation(user)
+    invitations.create(friend_id: user.id)
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_18_153513) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_23_214002) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "invitations", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.integer "friend_id"
+    t.boolean "confirmed", default: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_invitations_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "body"
@@ -38,5 +47,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_18_153513) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "invitations", "users"
   add_foreign_key "posts", "users"
 end
