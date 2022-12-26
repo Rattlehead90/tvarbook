0c248bc94f703d450b72f50076df6214e9bf562b
Rattlehead90 <strogonov@seznam.cz>
2022-12-26 12:49:35 +0100

    Add show profile option to pop div

--- app/javascript/controllers/logout_controller.js
+++ app/javascript/controllers/logout_controller.js
@@ -9,23 +9,39 @@ export default class extends Controller {
 
     this.stateValue++;
     if (this.stateValue % 2 != 0) {
+      // Parent pop div 
       const pop = document.createElement('div')
+      // Option elements inside the pop div
       const logout_pop = document.createElement('a');
       const edit_pop = document.createElement('a');
+      const show_pop = document.createElement('a');
+      // Positioning of pop div
       pop.id = 'pop';
       pop.style.position = 'absolute';
+      // Links' settings
       logout_pop.href = '/users/sign_out';
       edit_pop.href = '/users/edit';
+      show_pop.href = '/users/show';
+      // Options' labels
       logout_pop.textContent = 'Log out';
       edit_pop.textContent = 'Edit profile';
+      show_pop.textContent = 'Show profile';
+
+      // Styling of the pop div 
       pop.classList.add("shadow-xl", "border-2", "border-gray-50", "bg-white", 
                                "p-2", "w-48", "right-0", "top-12", "rounded-xl",
                                "flex", "flex-col");
+      // Adding of option elements
+      show_pop.classList.add("hover:bg-gray-300", "rounded-xl", "p-2");
       logout_pop.classList.add("hover:bg-gray-300", "rounded-xl", "p-2");
       edit_pop.classList.add("hover:bg-gray-300", "rounded-xl", "p-2");
 
+      // Appending options to pop div
+      pop.appendChild(show_pop);
       pop.appendChild(edit_pop);
       pop.appendChild(logout_pop);
+
+      // Connecting the pop div to avatar button on navbar
       avatar.appendChild(pop);
     } else {
       const pop = document.getElementById('pop');
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/show'
   resources :posts
   devise_for :users
+  resources :users, only: [:show]
 
   root 'posts#index'
 end
