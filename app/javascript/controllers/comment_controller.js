ad878f97bf83f3c8cf80287e324c9bee3ecf09c3
Rattlehead90 <strogonov@seznam.cz>
2023-01-04 15:45:02 +0100

    Scale comments

--- app/javascript/controllers/comment_controller.js
+++ app/javascript/controllers/comment_controller.js
@@ -1,7 +1,8 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-  static targets = [ 'input' ]
+  static targets = [ 'input', 'more' ]
+  static values = { state: { type: Number, default: 0 } }
   pulse() {
     const input = this.inputTarget;
     input.addEventListener('keydown', (e) => {
@@ -9,5 +10,25 @@ export default class extends Controller {
         input.classList.add('animate-pulse');
       }
     });
+  }
+
+  expand() {
+    const more = this.moreTarget;
+    const loaded_comments = document.querySelectorAll('p~div');
+
+    this.stateValue++
+    if (this.stateValue % 2 != 0) {
+      more.textContent = 'Show less'
+      loaded_comments.forEach(comment => {
+        comment.classList.remove('scale-0', 'absolute');
+        comment.classList.add('scale-1', 'relative');
+      });
+    } else {
+      more.textContent = `Show ${loaded_comments.length} more...`
+      loaded_comments.forEach(comment => {
+        comment.classList.add('scale-0', 'absolute');
+        comment.classList.remove('scale-1', 'relative');
+      });
+    };
   }
 }
