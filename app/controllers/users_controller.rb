08a89223782f2437427c814272b4eb78b714d40b
Rattlehead90 <strogonov@seznam.cz>
2023-01-12 21:12:13 +0100

    Fixed the bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,6 @@ class UsersController < ApplicationController
   def index
     @users = User.order(:last_name).page params[:page]
     @user = current_user
-    @invitation_not_sent = Invitation.request_sent?(current_user.id, @user.id)
-    @invitation = Invitation.find_invitation(@user.id, current_user.id)
   end
 
   def show
