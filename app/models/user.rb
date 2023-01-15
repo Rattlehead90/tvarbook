160175da5b56c6103dc5785df16ae7b3a9e078c7
Rattlehead90 <strogonov@seznam.cz>
2023-01-15 19:58:15 +0100

    Comment out skip confirmation on omniauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -45,7 +45,7 @@ class User < ApplicationRecord
       user.name = auth.info.name   # assuming the user model has a name
       # If you are using confirmable and the provider(s) you use validate emails, 
       # uncomment the line below to skip the confirmation emails.
-      user.skip_confirmation!
+      # user.skip_confirmation!
     end
   end
 end
