d8dbf83634b40069415e213bc3ac45405b01c7cd
Rattlehead90 <strogonov@seznam.cz>
2023-01-21 20:30:19 +0100

    Fix bugs with edit profile

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,8 @@ class ApplicationController < ActionController::Base
   protected
   
     def configure_permitted_parameters
-      devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :last_name, :dob, :country])
+      devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :last_name, :dob, :country)}
+
+      devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password, :last_name, :dob, :country)}
     end
 end
